6822e0e40421efce2626c90ef18c2bb11b0b2409
Wenfeng Hu <wenfeng.hu@ericsson.com>
2016-08-30 15:04:01 +0200

    [2016-08-30] ewenfhu compelte the 1st mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -34,13 +34,16 @@ clf = GaussianNB()
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
+print "training time of ", len(features_train), "training emails:", round(time()-t0, 3), "s"
 
+t1 = time()
 labels_predict = clf.predict(features_test)
-accuracy_score(labels_predict,labels_test)
+print "predicting time of", len(features_test), "emails:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_predict,labels_test)
 
 ### print out accuracy
-print "Accuracy of Naive Bayes algrorithm for email author classification:", accuracy_score
+print "Accuracy of Naive Bayes algrorithm for email author classification:", accuracy
 #########################################################
 
 
